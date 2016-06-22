499dc09661bcdcfd2feabfe42d994ad078b02cac
Timo Petteri Sinnemäki <timo.sinnemaki@helsinki.fi>
2016-06-22 22:57:29 +0300

    Removed 'defer' code from updateProblems.

--- src/Idris/Core/ProofState.hs
+++ src/Idris/Core/ProofState.hs
@@ -970,14 +970,6 @@ updateProblems ps updates probs = rec 10 updates probs
   ulog = unifylog ps
   usupp = map fst (notunified ps)
   dont = dontunify ps
-  defer = defer_solve ps -- TODO THINK FIXME Is this the right place for defer?
-                         -- Is it possible to leave the defer stuff to a later
-                         -- stage that actually drops the holes?
-                         -- - Study call sites of updateProblems.
-                         --   - processTactic EndUnify seems to filter out
-                         --     dontunify from its arguments to updateProblems,
-                         --     for example.
-                         --     - But are there others that don't do similar?
 
   up ns acc [] = (ns, map (updateNs ns) (reverse acc))
   up ns acc (prob@(x, y, ready, env, err, while, um) : ps) =
@@ -989,11 +981,8 @@ updateProblems ps updates probs = rec 10 updates probs
           if newx || newy || ready ||
              any (\n -> n `elem` inj) (refsIn x ++ refsIn y) then
             case unify ctxt env' (x', lp) (y', rp) inj hs usupp while of
-                 -- TODO THINK FIXME show 'defer' also?
                  OK (v, []) -> traceWhen ulog ("DID " ++ show (x',y',ready,v,dont)) $
-                                let v' = filter -- TODO THINK FIXME see above on defer.
-                                           (\(n, _) -> n `notElem` (dont ++ defer))
-                                           v in
+                                let v' = filter (\(n, _) -> n `notElem` (dont)) v in
                                     up (ns ++ v') acc ps
                  e -> -- trace ("FAILED " ++ show (x',y',ready,e)) $
                       up ns ((x',y',False,env',err',while,um) : acc) ps
